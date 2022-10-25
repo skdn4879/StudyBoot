0fe3232cf9ebb3aa62730c6ab34ec31d20e95a84
skdn4879 <skanxkrl3645@naver.com>
2022-10-25 17:47:18 +0900

    add_member_basic_transactional

--- src/main/java/com/iu/home/member/MemberService.java
+++ src/main/java/com/iu/home/member/MemberService.java
@@ -2,8 +2,10 @@ package com.iu.home.member;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(rollbackFor = Exception.class)
 public class MemberService {
 
 	@Autowired
@@ -12,11 +14,16 @@ public class MemberService {
 	public int setJoin(MemberVO memberVO) throws Exception {
 		boolean memberResult = memberMapper.setJoin(memberVO) == 1 ? true : false;
 		boolean roleResult = memberMapper.setRoleJoin(memberVO) == 1 ? true : false;
+		// 위 결과는 Exception이 발생해서 실패한 것이 아니다. 그냥 들어가면 1 아니면 0
 		
 		int result = 0;
 		
 		if (memberResult && roleResult) {
 			result = 1;
+		}
+		
+		if(result < 1) {
+			throw new Exception();
 		}
 		
 		return result;
